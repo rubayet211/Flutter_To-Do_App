ed82920cd1ec1b5af285e92b26e9b26bd897b1fb
rubayet211 <rubayet211@gmail.com>
2023-07-28 17:31:38 +0600

    6th commit

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color tdBGColor = Color(0xFFE5E5E5);
 const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF717171);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,27 +12,33 @@ class Home extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 15),
         child: Column(children: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(0),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: tdBlack,
-                  size: 20,
-                ),
-                prefixIconConstraints:
-                    BoxConstraints(maxHeight: 20, minWidth: 25),
-                border: InputBorder.none,
-                hintText: 'Search',
-              ),
-            ),
-          )
+          searchBox(),
         ]),
+      ),
+    );
+  }
+
+  Widget searchBox() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: tdBlack,
+              size: 20,
+            ),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(
+              color: tdGrey,
+            )),
       ),
     );
   }
