66d9a448fd2815bb351e365d3e900272900ce182
rubayet211 <rubayet211@gmail.com>
2023-07-28 22:43:05 +0600

    11th commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,9 +3,14 @@ import '../model/todo.dart';
 import '../constants/colors.dart';
 import '../widgets/todo_item.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final todoList = ToDo.todoList();
 
   @override
@@ -13,34 +18,99 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(children: [
-          searchBox(),
-          Expanded(
-            child: ListView(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 50, bottom: 20),
-                  child: Text(
-                    "All To-Do List",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(children: [
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      child: Text(
+                        "All To-Do List",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    for (ToDo todo in todoList)
+                      ToDoItem(
+                        todo: todo,
+                        onToDoChanged: _handleToDoChange,
+                        onDeleteItem: () {},
+                      ),
+                  ],
+                ),
+              )
+            ]),
+          ),
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        bottom: 20,
+                        right: 20,
+                        left: 20,
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0.0, 0.0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0,
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Add New To-DO List Item',
+                          border: InputBorder.none,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                for (ToDo todo in todoList)
-                  ToDoItem(
-                    todo: todo,
-                  ),
-              ],
-            ),
-          )
-        ]),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20, right: 20),
+                    child: ElevatedButton(
+                      child: Text(
+                        '+',
+                        style: TextStyle(
+                          fontSize: 40,
+                        ),
+                      ),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: tdBlue,
+                        minimumSize: Size(60, 60),
+                        elevation: 10,
+                      ),
+                    ),
+                  )
+                ],
+              ))
+        ],
       ),
     );
   }
+
+  void _handleToDoChange(ToDo todo) {
+    setState(() {});
+    todo.isDone = !todo.isDone!;
+  }
+
+  void _deleteToDoItem(String id) {}
 
   Widget searchBox() {
     return Container(
--- lib/widgets/todo_item.dart
+++ lib/widgets/todo_item.dart
@@ -4,7 +4,14 @@ import '../model/todo.dart';
 
 class ToDoItem extends StatelessWidget {
   final ToDo todo;
-  const ToDoItem({Key? key, required this.todo}) : super(key: key);
+  final onToDoChanged;
+  final onDeleteItem;
+  const ToDoItem(
+      {Key? key,
+      required this.todo,
+      required this.onToDoChanged,
+      required this.onDeleteItem})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +19,7 @@ class ToDoItem extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 20),
       child: ListTile(
         onTap: () {
-          print("Tapped");
+          onToDoChanged(todo);
         },
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(20),
@@ -20,7 +27,9 @@ class ToDoItem extends StatelessWidget {
         contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
         tileColor: Colors.white,
         leading: Icon(
-          Icons.check_box,
+          (todo.isDone ?? false)
+              ? Icons.check_box
+              : Icons.check_box_outline_blank,
           color: tdBlue,
         ),
         title: Text(
@@ -28,7 +37,8 @@ class ToDoItem extends StatelessWidget {
           style: TextStyle(
             fontSize: 16,
             color: tdBlack,
-            decoration: TextDecoration.lineThrough,
+            decoration:
+                (todo.isDone ?? false) ? TextDecoration.lineThrough : null,
           ),
         ),
         trailing: Container(
