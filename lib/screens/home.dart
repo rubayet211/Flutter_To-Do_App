d32283f37c0e5c25dde02c3ee92a5b87d6fa6190
rubayet211 <rubayet211@gmail.com>
2023-07-28 17:49:12 +0600

    6th commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,9 +10,25 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
         child: Column(children: [
           searchBox(),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50, bottom: 20),
+                  child: Text(
+                    "All To-Do List",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
         ]),
       ),
     );
