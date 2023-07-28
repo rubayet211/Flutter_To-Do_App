3cb3a831fc230213193ebdf73f4ff05ad922422d
rubayet211 <rubayet211@gmail.com>
2023-07-28 17:06:16 +0600

    3rd Commit

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,9 +7,29 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("To-Do App")),
-        body: Container(
-          child: Text('This is home screen'),
-        ));
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpeg'),
+            ),
+          )
+        ]),
+      ),
+      body: Container(
+        child: Text('This is home screen'),
+      ),
+    );
   }
 }
