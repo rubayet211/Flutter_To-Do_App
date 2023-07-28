128395ddb14badd88ef64aa821ce3893f23e1aa7
rubayet211 <rubayet211@gmail.com>
2023-07-29 01:04:42 +0600

    14th commit

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color tdBGColor = Color(0xFFE5E5E5);
+const Color tdBGColor = Color(0xFFF3FFF7);
 const Color tdBlack = Color(0xFF3A3A3A);
-const Color tdGrey = Color(0xFF717171);
-const Color tdBlue = Color(0xFF5F52EE);
-const Color tdRed = Color(0xFFDA4040);
+const Color tdGrey = Color.fromARGB(255, 120, 120, 120);
+const Color tdBlue = Color.fromARGB(255, 82, 181, 238);
+const Color tdRed = Color.fromARGB(255, 245, 76, 76);
